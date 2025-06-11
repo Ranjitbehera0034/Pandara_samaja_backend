b86b4299f0edd550d78951b6c85b845025190e06
Ranjitbehera0034 <ranjitbehera0034@gmail.com>
2025-06-11 18:46:45 +0530

    updated cors

--- app.js
+++ app.js
@@ -3,8 +3,30 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+
+// server.js (or app.js)
+
 const app = express();
-app.use(cors());
+const allowedOriginsDev = [
+  'http://localhost:5000', // update with your dev origin(s)
+  'http://127.0.0.1:5000'
+];
+const allowedOriginsProd = [
+  'https://pandara-samaja-backend.onrender.com' // update with your production origin(s)
+];
+const allowedOrigins = process.env.NODE_ENV === 'production' ? allowedOriginsProd : allowedOriginsDev;
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow REST tools like Postman which send no Origin header
+    if (!origin || origin === 'null') return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      return callback(null, true);
+    }
+    callback(new Error('Not allowed by CORS: ' + origin));
+  }
+}));
 app.use(bodyParser.json());
 
 const candidateRoutes = require('./routes/candidateRoutes');
