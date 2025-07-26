e291b143014bb75d6d63819519863c3791858c89
Ranjitbehera0034 <ranjitbehera0034@gmail.com>
2025-07-26 12:26:12 +0530

    update member routes

--- routes/memberRoutes.js
+++ routes/memberRoutes.js
@@ -8,9 +8,9 @@ const memberController = require('../controllers/memberController');
 const router = express.Router();
 
 // File upload endpoint (already expected by importExcel)
-router.post('/members/import', upload.single('file'), memberController.importExcel);
+router.post('/import', upload.single('file'), memberController.importExcel);
 
 // New: JSON rows endpoint
-router.post('/members/import-rows', memberController.importRows);
+router.post('/import-rows', memberController.importRows);
 
 module.exports = router;
