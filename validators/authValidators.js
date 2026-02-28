6a926b301d7ba931299cb248ca5e6ca922b3be10
ranjitbehera0034@gmail.com <ranjitbehera0034@gmail.com>
2026-02-28 23:24:57 +0530

    chore: Install project dependencies and update the QUICK_START.md documentation.

--- validators/authValidators.js
+++ validators/authValidators.js
@@ -5,6 +5,8 @@ const registerAdminSchema = z.object({
     username: z.string().min(3, 'Username must be at least 3 characters long').max(50),
     password: z.string().min(8, 'Password must be at least 8 characters long'),
     role: z.enum(['admin', 'super_admin']).optional(),
+    membership_no: z.string().min(1, 'Membership Number is required'),
+    real_name: z.string().optional(),
     email: z.string().email('Invalid email address').optional().or(z.literal('')),
 });
 
